b344eacf578670f29b259ae7cb9a4a41adf06610
gabrielaugustodeveloper <augusto.dev.g@gmail.com>
2025-08-07 22:08:37 -0300

    .

---
+++ Turma 29 módulo 01/week01/operadores_numericos.py
@@ -0,0 +1,12 @@
+numero_inteiro = 10
+numero_decimal = 7.5
+
+nome = "Augusto"
+
+print(type(numero_inteiro))
+print(type(numero_decimal))
+print(type(nome))
+
+idade = 18
+cidade = "Recife"
+print(f"Meu nome é {nome}, tenho {idade} anos e moro em {cidade}.")
